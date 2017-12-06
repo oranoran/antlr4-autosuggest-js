ff52d907739cc41e17e3028be439e8f923486e62
Oran Epelbaum <oran.epelbaum@intigua.com>
2017-12-06 16:55:56 +0200

    updated ported Java code, still low coverage

--- tokensuggester.js
+++ tokensuggester.js
@@ -14,13 +14,8 @@ TokenSuggester.prototype.suggest = function (parserState, remainingText) {
     var lexerState = this._toLexerState(parserState);
     if (lexerState == null) {
         return this._suggestions;
-    } else if (lexerState.transitions.length === 0) {
-        parserState.transitions.forEach((transition) => {
-            if (transition.serializationType === 5) { // AtomTransition
-                lexerState = this._toLexerState(transition.target);
-                this._suggest('', lexerState, remainingText);
-            }
-        });
+    } else if (lexerState.transitions.length === 0) { // at end of token
+        this._suggestViaParserTransition(parserState, remainingText);
     } else {
         this._suggest('', lexerState, remainingText);
     }
@@ -35,32 +30,60 @@ TokenSuggester.prototype._toLexerState = function (parserState) {
     return lexerState;
 };
 
-TokenSuggester.prototype._suggest = function (completionSoFar, state, remainingText) {
-    if (state.stateNumber in this._visitedLexerStates) {
+TokenSuggester.prototype._suggest = function (completionSoFar, lexerState, remainingText) {
+    if (lexerState.stateNumber in this._visitedLexerStates) {
         return; // avoid infinite loop and stack overflow
     }
-    this._visitedLexerStates.push(state.stateNumber);
+    this._visitedLexerStates.push(lexerState.stateNumber);
     try {
-        if (state.transitions.length === 0 && completionSoFar.length > 0 && !this._suggestions.includes(completionSoFar)) {
+        if (lexerState.transitions.length === 0 && completionSoFar.length > 0 && !this._suggestions.includes(completionSoFar)) {
             this._suggestions.push(completionSoFar);
             return;
         }
-        state.transitions.forEach((trans) => {
-            if (trans.isEpsilon) {
-                this._suggest(completionSoFar, trans.target, remainingText);
-            } else if (trans.serializationType === 5) { // AtomTransition
-                var transitionToken = this._getAddedTextFor(trans);
-                if (transitionToken.startsWith(remainingText)) {
-                    var newTransitionToken = this._chopOffCommonStart(transitionToken, remainingText);
-                    var newRemainingText = this._chopOffCommonStart(remainingText, transitionToken);
-                    this._suggest(completionSoFar + newTransitionToken, trans.target, newRemainingText);
-                }
-            }
+        lexerState.transitions.forEach((trans) => {
+            this._suggestViaLexerTransition(completionSoFar, remainingText, trans);
         });
     } finally {
         this._visitedLexerStates.pop();
     }
 };
+
+TokenSuggester.prototype._suggestViaLexerTransition = function(completionSoFar, remainingText, trans) {
+    if (trans.isEpsilon) {
+        this._suggest(completionSoFar, trans.target, remainingText);
+    } else if (trans.serializationType === 5) { // AtomTransition
+        var transitionToken = this._getAddedTextFor(trans);
+        if (transitionToken.startsWith(remainingText)) {
+            this._suggestViaNonEpsilonLexerTransition(completionSoFar, remainingText, transitionToken, trans.target);
+        }
+    } else if (trans.serializationType === 7) { // SetTransition
+        var symbols = trans.label;
+        symbols.forEach((symbol)=>{
+            var transitionToken = symbol;
+            if(transitionToken.startsWith(remainingText)) {
+                this._suggestViaNonEpsilonLexerTransition(completionSoFar, remainingText, transitionToken, trans.target);
+            }
+        });
+    }
+}
+
+TokenSuggester.prototype._suggestViaNonEpsilonLexerTransition = function(completionSoFar, remainingText, transitionToken, targetState) {
+    var newTransitionToken = this._chopOffCommonStart(transitionToken, remainingText);
+    var newRemainingText = this._chopOffCommonStart(remainingText, transitionToken);
+    this._suggest(completionSoFar + newTransitionToken, targetState, newRemainingText);
+}
+
+TokenSuggester.prototype._suggestViaParserTransition = function(parserState, remainingTest) {
+    parserState.transitions.forEach((transition) => {
+        if (transition.isEpsilon) {
+            this._suggestViaParserTransition(transition.target, remainingTest);
+        }
+        else if (transition.serializationType === 5) { // AtomTransition
+            lexerState = this._toLexerState(transition.target);
+            this._suggest('', lexerState, remainingText);
+        }
+    });
+}
 
 TokenSuggester.prototype._chopOffCommonStart = function (remainingText, transitionToken) {
     var charsToChopOff = Math.min(transitionToken.length, remainingText.length);
